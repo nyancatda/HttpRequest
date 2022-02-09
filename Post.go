de689ff8c312993e86eb212b4460f1276f253e7f
NyanCatda <nyancatda@outlook.com>
2022-02-09 20:41:09 +0800

    统一参数位置

--- Post.go
+++ Post.go
@@ -1,7 +1,7 @@
 /*
  * @Author: NyanCatda
  * @Date: 2022-02-09 20:30:52
- * @LastEditTime: 2022-02-09 20:34:13
+ * @LastEditTime: 2022-02-09 20:40:29
  * @LastEditors: NyanCatda
  * @Description: Post请求方法封装
  * @FilePath: \HttpRequest\Post.go
@@ -19,13 +19,13 @@ import (
 /**
  * @description: POST请求封装，传递Json
  * @param {string} url 请求地址
- * @param {string} requestBody 请求内容(Json)
  * @param {[]string} Header 请求头
+ * @param {string} requestBody 请求内容(Json)
  * @return {[]byte} 返回内容
  * @return {*http.Response} 请求响应信息
  * @return {error} Error
  */
-func PostRequestJson(url string, requestBody string, Header []string) ([]byte, *http.Response, error) {
+func PostRequestJson(url string, Header []string, requestBody string) ([]byte, *http.Response, error) {
 	var jsonStr = []byte(requestBody)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	if err != nil {
